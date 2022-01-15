c8396ddaf8a13ec17a2170f02cb797ce96ef4ac8
TheHeroOftacos <markussha123@gmail.com>
2022-01-15 21:03:04 +0200

    db

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from flask import Flask, request, flash, url_for, redirect, render_template
-import sqlalchemy as db
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
 
 app = Flask(__name__)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
-
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-class login(db.Model):
+@app.route('/')
+def index():
+   return render_template("home.html")
+class User(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    username = db.Column(db.String(80), unique=True, nullable=False)
    password = db.Column(db.String(120), unique=True, nullable=False)
+   
+   def __repr__(self):
+      return '<User %r>' % self.username
 
-def __init__(self):
-   return '<User %r>' % self.username
+
+if __name__ == "__main__":
+   app.run(debug=True)
