a88e32eddfd6298de8288d33043865438647d689
TheHeroOftacos <markussha123@gmail.com>
2022-01-20 22:46:39 +0200

    s

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-from venv import create
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+# from venv import create
+# from flask import Flask, render_template, url_for
+# from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__, template_folder="template")
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
+# app = Flask(__name__, template_folder="template")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# db = SQLAlchemy(app)
 
 
-@app.route('/')
-def index():
-   return render_template("home.html")
-class User(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String(80), unique=True, nullable=False)
-   password = db.Column(db.String(120), unique=True, nullable=False)
+# @app.route('/')
+# def index():
+#    return render_template("home.html")
+# class User(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    username = db.Column(db.String(80), unique=True, nullable=False)
+#    password = db.Column(db.String(120), unique=True, nullable=False)
+   
+#    def __repr__(self):
+#         return '<User %r>' % self.username
 
-@app.route('/admin')
-def admin():
-    return render_template("end.html")
+# @app.route('/admin')
+# def admin():
+#     return render_template("end.html")
 
 
-if __name__ == "__main__":
-   app.run(debug=True)
+# if __name__ == "__main__":
+#    # app.run(debug=True)
