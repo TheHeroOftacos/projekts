e7854409b4354a66c9b6f7009190b7a19ef8ea93
TheHeroOftacos <markussha123@gmail.com>
2022-01-25 09:30:01 +0200

    notes

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def login():
 #problēma - 
 #Not Found 
 #The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.
+#vajag salabot login pogu lai viņš sūta uz end.html
 @app.route('/')
 @app.route('/home')
 def index():
