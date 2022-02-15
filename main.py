9a7a654395b436f668bdd57ad7d1f57d0c940b7f
TheHeroOftacos <markussha123@gmail.com>
2022-02-15 11:23:36 +0200

    s\

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
-
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
-from wtforms.validators import InputRequired, Length, Regexp, NumberRange
-from datetime import date
-
 
 
 # Create new flask session
@@ -21,30 +14,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
 # Create db connection
 db = SQLAlchemy(app)
 
-class Esana(FlaskForm):
-    # id used only by update/edit
-    id_field = HiddenField()
-    vards = StringField('vārds Uzvārds')
-    klase = SelectField('Izvēlies savu klasi',
-        choices=[ ('', ''), ('7a', '7A'),
-        ('7b', '7B'),
-        ('8a', '8A'),
-        ('8b', '8B'), 
-        ('9a', '9A'),
-        ('9b', '9B'),
-        ('10vid', '10VID'),
-        ('10sp', '10SP'),
-        ('10vv', '10VV'),
-        ('11id', '11ID'),
-        ('11sp', '11SP'),
-        ('11vv', '11VV'),
-        ('12ek', '12EK'),
-        ('12ip', '12IP'),
-        ('12dz', '12DZ') ])
-    diena =  RadioField('dienas', choices=[('1','Pirmdiena'),('2','Otrdiena'),('3','Trešdiena'),('4','Ceturtdiena'),('5','Piektdiena')])
-    # updated - date - handled in the route function
-    updated = HiddenField()
-    submit = SubmitField('Iesniegt')
+# class Esana(FlaskForm):
+#     # id used only by update/edit
+#     id_field = HiddenField()
+#     vards = StringField()
+#     # klase = SelectField('Izvēlies savu klasi',
+#     #     choices=[ ('', ''), ('7a', '7A'),
+#     #     ('7b', '7B'),
+#     #     ('8a', '8A'),
+#     #     ('8b', '8B'), 
+#     #     ('9a', '9A'),
+#     #     ('9b', '9B'),
+#     #     ('10vid', '10VID'),
+#     #     ('10sp', '10SP'),
+#     #     ('10vv', '10VV'),
+#     #     ('11id', '11ID'),
+#     #     ('11sp', '11SP'),
+#     #     ('11vv', '11VV'),
+#     #     ('12ek', '12EK'),
+#     #     ('12ip', '12IP'),
+#     #     ('12dz', '12DZ') ])
+#     # diena =  RadioField('dienas', choices=[('1','Pirmdiena'),('2','Otrdiena'),('3','Trešdiena'),('4','Ceturtdiena'),('5','Piektdiena')])
+#     # # updated - date - handled in the route function
+#     # updated = HiddenField()
+#     # submit = SubmitField('Iesniegt')
 
 class User(db.Model):
    id = db.Column(db.Integer, primary_key=True)
@@ -60,15 +53,15 @@ class est(db.Model):
     __tablename__ = 'socks'
     id = db.Column(db.Integer, primary_key=True)
     vards = db.Column(db.String)
-    klase = db.Column(db.String)
-    diena = db.Column(db.String)
-    updated = db.Column(db.String)
+    # klase = db.Column(db.String)
+    # diena = db.Column(db.String)
+    # updated = db.Column(db.String)
 
-    def __init__(self, vards, klase, diena, updated):
+    def __init__(self, vards):
         self.vards = vards
-        self.klase = klase
-        self.diena = diena
-        self.updated = updated
+        # self.klase = klase
+        # self.diena = diena
+        # self.updated = updated
 
 
 @app.route('/admin')
@@ -97,17 +90,17 @@ def login():
 #The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.
 #vajag salabot login pogu lai viņš sūta uz end.html
 
-@app.route("/paldies", methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
         vards = request.form['vards']
 
-        ediens = est(vards = vards)
+        article = est(vards = vards)
 
         try:
-            db.session.add(ediens)
+            db.session.add(article)
             db.session.commit()
-            return redirect('/admin')
+            return redirect('/home')
         except:
             return "notika klume"
     else:
